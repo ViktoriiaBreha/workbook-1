5cd9ee6fb5142abbd28b1b27cdf03c4653313da1
Viktoriia Breha <viktoriia.breha@gmail.com>
2025-04-09 11:37:35 -0400

    completed ShoppingList app

---
+++ first-test-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Apples, Butter, Bread, Meat, Strawberries, Cherries, Cookies, Eggs, Salt, Sugar");
+    }
+}
