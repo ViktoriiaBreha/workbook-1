6a642f3669a2eb8409616180d25cc81fbe0a7e61
Viktoriia Breha <viktoriia.breha@gmail.com>
2025-04-10 15:39:59 -0400

    Exercise 4 with If

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -14,7 +14,17 @@ public class PayrollCalculator {
         System.out.print("Enter your pay rate: ");
         float pay_rate = scanner.nextFloat();
 
-        float gross_pay = hours * pay_rate;
+        float gross_pay;
+
+
+        if (hours > 40) {
+            float regular_payment = 40 * pay_rate;
+            float overtime = (hours - 40) * (pay_rate * 1.5f);
+            gross_pay = regular_payment + overtime;
+        } else {
+            gross_pay = hours * pay_rate;
+        }
+
 
         System.out.printf("Name: %s\n", name);
         System.out.printf("Gross pay: %.2f\n", gross_pay);
